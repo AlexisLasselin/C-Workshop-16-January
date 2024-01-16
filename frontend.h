8fa2315630e1e56eed880f4e4416e240de2e1f81
arthur-lemo1ne <arthur.lemoine@algosup.com>
2024-01-16 15:20:59 +0100

    Update

--- frontend.h
+++ frontend.h
@@ -1,9 +1,12 @@
+#include <string.h>
+
 typedef struct Product
 {
     int id;
     char name[50];
     int quantity;
     double price;
+    int index;
 }T_Product;
 
 T_Product CreateProduct(int xid, char xname[50], int xquantity, double xprice)
@@ -17,4 +20,60 @@ T_Product CreateProduct(int xid, char xname[50], int xquantity, double xprice)
     arival.quantity = xquantity;
     arival.price = xprice;
     return arival;
+}
+
+void DisplayProduct(T_Product product)
+{
+    if(product.index != -1)
+    {
+        printf("=====Product=====\n");
+        printf("ID: %d\n", product.id);
+        printf("Name: %s\n", product.name);
+        printf("Quantity: %d\n", product.quantity);
+        printf("Price: %lf\n", product.price);
+        printf("=================\n");
+    } 
+
+}
+
+typedef struct DataBase
+{
+    T_Product db[1000];
+    int NumberOfProduct;
+}T_DataBase;
+
+void AddProduct(T_DataBase* xdb, T_Product xproduct)
+{
+    xproduct.index = xdb->NumberOfProduct;
+    xdb->db[xdb->NumberOfProduct] = xproduct;
+    xdb->NumberOfProduct = xdb->NumberOfProduct+1;
+}
+
+T_Product Search(T_DataBase* xdb, char xname[50])
+{
+    T_Product back;
+    back.index = -1;
+    for(int i = 0 ; i < xdb->NumberOfProduct; i++)
+    {
+        if(!strcmp(xdb->db[i].name, xname))
+        {
+            return xdb->db[i];
+        }
+    }
+    printf("ERROR: Product Not Found\n");
+    return back;
+}
+
+void DeleteProduct(T_DataBase* xdb, char xname[50])
+{
+    T_Product x;
+    x = Search(xdb, xname);
+    if(x.index != -1)
+    {
+        for(int i = x.index; i < xdb->NumberOfProduct+1; i++)
+        {
+            xdb->db[i] = xdb->db[i+1];
+        }
+        printf("Product Deleted\n");
+    }
 }
--- main.c
+++ main.c
@@ -4,6 +4,21 @@
 #include "backend.h"
 
 int main() {
-    printf("Hello World!\n");
+    
+    T_DataBase DB;
+    DB.NumberOfProduct = 0;
+    
+    T_Product x1 = CreateProduct(1, "pasta", 50, 2.20);
+    T_Product x2 = CreateProduct(2, "rice", 45, 2.50);
+
+
+    AddProduct(&DB, x1);
+    AddProduct(&DB, x2);
+
+    DisplayProduct(Search(&DB, "rice"));
+    DeleteProduct(&DB, "rice");
+    DisplayProduct(Search(&DB, "rice"));
+
+
     return EXIT_SUCCESS;
 }
