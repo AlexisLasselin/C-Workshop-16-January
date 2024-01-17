f9569896bf5603796be5a03ffade29977db26b04
PaulNowak36 <91249965+PaulNowak36@users.noreply.github.com>
2024-01-17 16:33:11 +0100

    Update unitTestingBackend.c
    Set up the field for backend test

---
+++ unitTestingBackend.c
@@ -0,0 +1,17 @@
+#include <assert.h>
+#include "backend.h"
+
+int main(void)
+{
+    //UNIT TEST 1: CREATE A NEW CSV FILE
+        //CODE
+    //
+
+    //UNIT TEST 2: REMOVE CSV FILE
+        //CODE
+    //
+
+    //UNIT TEST 3: SAVE A CSV FILE
+        //CODE
+    //
+}
